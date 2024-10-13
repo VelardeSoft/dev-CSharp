d9801f6494fa2876705437a03d78aa7d16f4a3ba
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-10-12 22:50:54 -0500

    feat: Implementación de arreglos.

--- dev-CSharp/Program.cs
+++ dev-CSharp/Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace CSharpHelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // Hola Mundo con C#
+            Console.WriteLine("Hola, C#.");
+            
+            // Datos de tipo cadena
+            string myString = "Esto es una cadena de texto"; 
+            Console.WriteLine(myString);
+            
+            // Datos de tipo entero 
+            int myInt = 7;
+            myInt = myInt + 4; 
+            Console.WriteLine(myInt);
+            Console.WriteLine(myInt - 1);
+            
+            // Datos de tipo decimal 
+            double myFloat = 6.5; 
+            Console.WriteLine(myFloat);
+            
+            //Operaciones 
+            Console.WriteLine(myFloat + myInt);
+            
+            //Dinamic 
+            dynamic myDynamic = 6;
+            myDynamic = "Mi dato dinamico"; 
+            Console.WriteLine(myDynamic);
+            
+            // var es un string
+            var myVar = "Mi variable de tipo inferido"; 
+            Console.WriteLine(myVar);
+            
+            //boleano
+            bool myBool = false; 
+            Console.WriteLine(myBool);
+            
+            // Interpolar tipos de datos
+            Console.WriteLine($"Esta es {myBool} y {myDynamic}");
+            
+            // Constante
+            const string myConst = "Mi constante"; 
+            Console.WriteLine(myConst);
+            
+            // Estructuras para guardar mas de un valor 
+            //Listado de componentes o arreglos
+            var myArray = new string[5] {"Teresita", "Kony", "Mamita", "Messo", "Velarde"}; 
+            Console.WriteLine(myArray[0]);
+        }
+    }
+}
+
---
+++ dev-CSharp/README.md
@@ -0,0 +1,16 @@
+﻿Comando para crear un proyecto de consola en C#:
+
+dotnet new console 
+
+Comando para ejecutar un proyecto de consola en C#:
+
+dotnet run
+
+Comando para crear un proyecto de librería en C#:
+
+dotnet new classlib
+
+Comando para compilar un proyecto de C#:
+
+dotnet build
+
