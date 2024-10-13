672d8c35ae57f5279a5a8ee0f47f0b7f223bab75
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-10-12 23:32:58 -0500

    feat: Implementación de bucles y condiciones.

--- dev-CSharp/Program.cs
+++ dev-CSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace CSharpHelloWorld
 {
@@ -49,8 +50,102 @@ namespace CSharpHelloWorld
             // Estructuras para guardar mas de un valor 
             //Listado de componentes o arreglos
             var myArray = new string[5] {"Teresita", "Kony", "Mamita", "Messo", "Velarde"}; 
-            Console.WriteLine(myArray[0]);
+            Console.WriteLine(myArray[2]);
+            
+            // Diccionary
+            var myDictionary = new Dictionary<string, int>  // Clave - valor 
+            {
+                { "Velarde", 24 },
+                { "Teresita", 23 },
+                { "Mamita", 24 }
+            }; 
+            Console.WriteLine(myDictionary["Velarde"]);
+            
+            // Estructura Set, son estructuras desordenadas, los valores repetidos se ovbian
+            var mySet = new HashSet<string> { "Teresita", "Messo", "Velarde" }; 
+            Console.WriteLine(mySet);
+            
+            // Tuplas
+            var myTuple = ("Teresita", "Messo", "Velarde");
+            Console.WriteLine(myTuple);
+            
+            //Bucles 
+            Console.WriteLine("---- BUCLES ----");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---- ForEach ----");
+            int num = 1; 
+            foreach (var myItem in myArray)
+            {
+                Console.WriteLine($"{num} : {myItem}");
+                num++;
+            }
+
+            myInt = 0; 
+            //Flujos
+            if (myInt == 11 && myBool == true)
+            {
+                Console.WriteLine("El valor es 11");
+            } else if (myInt == 0 || myBool == false)
+            {
+                Console.WriteLine("El valor es CERO");
+            }
+            else
+            {
+                Console.WriteLine("El valor NO es 11");
+            }
+            
+            // Funciones 
+            MyFunction(); 
+            Console.WriteLine(MyFunctionWithReturn(5));
+            
+            // clases
+            var MyClass = new MyClass("Teresita");
+            Console.WriteLine(MyClass.myName);
+        }
+
+        static void MyFunction()
+        {
+            Console.WriteLine("Esta es mi funcion");
+        }
+        static int MyFunctionWithReturn(int param)
+        {
+            return 10 + param; 
+        }
+        
+        // Clases son objetos personalizados
+        class MyClass
+        {
+            public string myName { get; set; }
+
+            public MyClass(string myCurrentName)
+            {
+                myName = myCurrentName;
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
